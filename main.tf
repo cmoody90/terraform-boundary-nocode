4c679d0b48cc31d6d93eeb8ad60a2c06b355587a
cmoody90 <119501907+cmoody90@users.noreply.github.com>
2024-03-30 15:35:35 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "ssh-target" {
-  ami                  = "ami-0688ba7eeeeefe3cd"
+  ami                  = "ami-0a70b9d193ae8a799"
   instance_type        = "t2.micro"
   key_name = "boundary-vault-keypair"
   security_groups = ["SSH"]
