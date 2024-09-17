d21065c14a2074329b80d26d80f9c479bd888be4
cmoody90 <119501907+cmoody90@users.noreply.github.com>
2024-09-17 14:30:07 +0100

    Update subnet id

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ resource "aws_instance" "ssh-target" {
   instance_type        = "t2.micro"
   key_name             = "boundary-vault-keypair"
   vpc_security_group_ids = ["sg-0d3f1e99f596b7655"]
+  subnet_id              = "subnet-0aea8366156988c03" 
   tags = {
     Name = var.vm_name
   }
