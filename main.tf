4fab9be33307ceefa8bb00bf2dc0b0726b965a3f
cmoody90 <119501907+cmoody90@users.noreply.github.com>
2024-09-17 14:59:59 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 terraform {
   required_providers {
     boundary = {
@@ -8,49 +7,50 @@ terraform {
   }
 }
 
-#Deploy AWS Target
+# AWS Provider
 provider "aws" {
-    access_key = var.access_key
-    secret_key = var.secret_key
-    region = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
 }
 
+# EC2 Instance
 resource "aws_instance" "ssh-target" {
-  ami                  = "ami-05134c8ef96964280"
-  instance_type        = "t2.micro"
-  key_name             = "boundary-vault-keypair"
+  ami                    = "ami-05134c8ef96964280"
+  instance_type          = "t2.micro"
+  key_name               = "boundary-vault-keypair"
   vpc_security_group_ids = ["sg-0d3f1e99f596b7655"]
-  subnet_id              = "subnet-0aea8366156988c03" 
+  subnet_id              = "subnet-0aea8366156988c03"
   tags = {
     Name = var.vm_name
   }
 }
 
-#Configure and add target to Boundary
+# Boundary Provider
 provider "boundary" {
-  addr                            = var.url
-  auth_method_login_name = var.username          # changeme
-  auth_method_password   = var.password       # changeme
+  addr                   = var.url
+  auth_method_login_name = var.username
+  auth_method_password   = var.password
 }
 
-# Define a Host
-resource "boundary_host" "ssh_host" {
+# Define a Static Host
+resource "boundary_host_static" "ssh_host" {
   type            = "static"
   name            = "ssh-host"
   description     = "SSH Host for EC2 instance"
   address         = aws_instance.ssh-target.public_ip
-  host_catalog_id = "hcst_94IxZYoE6B"  # Replace with your Host Catalog ID
+  host_catalog_id = "hcst_94IxZYoE6B"
 }
 
-# Create a Host Set
-resource "boundary_host_set" "ssh_host_set" {
+# Create a Static Host Set
+resource "boundary_host_set_static" "ssh_host_set" {
   type            = "static"
   name            = "ssh-host-set"
   description     = "Host Set for SSH Hosts"
-  host_catalog_id = "hcst_94IxZYoE6B"  # Same Host Catalog ID
+  host_catalog_id = "hcst_94IxZYoE6B"
 
   host_ids = [
-    boundary_host.ssh_host.id
+    boundary_host_static.ssh_host.id
   ]
 }
 
@@ -61,10 +61,8 @@ resource "boundary_target" "target" {
   type                                      = "ssh"
   default_port                              = "22"
   scope_id                                  = var.scope_id
-  host_set_id                               = boundary_host_set.ssh_host_set.id
+  host_set_ids                              = [boundary_host_set_static.ssh_host_set.id]
   injected_application_credential_source_ids = [
     var.cred_id
   ]
 }
-
-
